b2ae07ebce94a4732c393d4bf8cff10c99041b6a
dmb-2222 <dmb-2222@yandex.ru>
2019-11-20 19:31:37 +0200

    Updates

--- precache-manifest.6e6f4f0677e33d93b363895c8fdf14b9.js
+++ precache-manifest.6e6f4f0677e33d93b363895c8fdf14b9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dc6117fea347084fd9901aa152dc9c60",
+    "revision": "232459840af7069e9ab8fcab2e8d4b0c",
     "url": "/goit-react14-hw-03-bank-account/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react14-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
   },
   {
-    "revision": "3f84679c0c174ddec97a",
+    "revision": "aaaf783a626cf760a0c8",
     "url": "/goit-react14-hw-03-bank-account/static/css/main.421524f3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react14-hw-03-bank-account/static/js/2.fda7b620.chunk.js"
   },
   {
-    "revision": "3f84679c0c174ddec97a",
-    "url": "/goit-react14-hw-03-bank-account/static/js/main.25f764d6.chunk.js"
+    "revision": "aaaf783a626cf760a0c8",
+    "url": "/goit-react14-hw-03-bank-account/static/js/main.62f176c6.chunk.js"
   },
   {
     "revision": "465b81e1ce568bba01e7",
